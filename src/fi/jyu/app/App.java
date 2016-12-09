3c29ff09856c7dc8737c1600a6cf2b026b20541c
Mikko Lampi <mikko@mikkolampi.fi>
2016-12-09 13:51:43 +0200

    better version which handles the 100 tweet limit of search API.

--- src/fi/jyu/app/App.java
+++ src/fi/jyu/app/App.java
@@ -13,8 +13,10 @@ public class App {
 		Config config = new Config();
 		Client client = null;
 		
+		args = new String[] {"search", "#Trump", "europe"};
+		
 		if (args.length == 0) {
-			System.out.println("twelper.jar [config|test|search] <what to search> <how many tweets>");
+			System.out.println("twelper.jar [config|test|search] <what to search> <geolocation with a radius>");
 			System.exit(0);
 		}
 		
@@ -39,21 +41,28 @@ public class App {
 			
 			if (null != args[1] && !args[1].isEmpty()
 					&& null != args[2] && !args[2].isEmpty()) {
+				
+				String geocode = "";
+				
+				if (args[2].equalsIgnoreCase("usa")) geocode = "39.8,-95.583068847656,2500km";
+				if (args[2].equalsIgnoreCase("europe")) geocode = "51.0504,13.7373,2300km";
+				
 				try {
-					client.search(args[1], Integer.parseInt(args[2]));
+					client.search(args[1], geocode);
 					System.out.println("Done.");
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}				
 			} else {
-				System.out.println("Don't know what to search and how many tweets to return.");
+				System.out.println("Don't know what to search and from which location.");
 			}
 			break;
-		
+
 		default:
 			System.out.println("Usage");
-			System.out.println("twelper.jar [config|test|search] <what to search> <how many tweets>");
+			System.out.println("twelper.jar <mode[config|test|search]> <search terms> <area[usa|europe]>");
+			System.out.println("Will fetch 1000 (or less) tweets based on the criteria.");
 		}
 	}
 
--- src/fi/jyu/data/twitter/Client.java
+++ src/fi/jyu/data/twitter/Client.java
@@ -6,14 +6,18 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Iterator;
 
 import javax.net.ssl.HttpsURLConnection;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import fi.jyu.util.CommHelpers;
+
 
 public class Client {
 
@@ -23,31 +27,62 @@ public class Client {
 		this.config = config;
 	}
 	
-	public void search(String keyword, int count) throws IOException {
+	public void search(String keyword, String geocode) throws IOException {
 		HttpsURLConnection connection = null;
 		
-		String request = "https://api.twitter.com/1.1/search/tweets.json?q=" + URLEncoder.encode(keyword, "UTF-8") + "&count=" + count;
-		
+		String request = "https://api.twitter.com/1.1/search/tweets.json?q=" + URLEncoder.encode(keyword, "UTF-8") 
+			+ "&count=100" + "&geocode=" + geocode + "&lang=en";
+
 		try {
-			URL url = new URL(request); 
-			connection = (HttpsURLConnection) url.openConnection();           
-			connection.setDoOutput(true);
-			connection.setDoInput(true); 
-			connection.setRequestMethod("GET"); 
-			connection.setRequestProperty("Host", "api.twitter.com");
-			connection.setRequestProperty("User-Agent", "Data Challenge");
-			connection.setRequestProperty("Authorization", "Bearer " + config.getBearerToken());
-			connection.setUseCaches(false);
+			URL url = null;
+			String response = "";
+			String results = "";
+			long maxId = 0;
+	
+			for (int i = 0; i < 10; i++) {
+				if (i > 0) request = "https://api.twitter.com/1.1/search/tweets.json?q=" 
+						+ URLEncoder.encode(keyword, "UTF-8") 
+						+ "&count=100" + "&geocode=" + geocode + "&lang=en&max_id=" + maxId;
 				
-			File f = new File("twitter_data.json");
-			if (f.exists()) f.delete();
+				url = new URL(request);
+				connection = (HttpsURLConnection) url.openConnection();           
+				connection.setDoOutput(true);
+				connection.setDoInput(true); 
+				connection.setRequestMethod("GET"); 
+				connection.setRequestProperty("Host", "api.twitter.com");
+				connection.setRequestProperty("User-Agent", "Data Challenge");
+				connection.setRequestProperty("Authorization", "Bearer " + config.getBearerToken());
+				connection.setUseCaches(false);				
+				
+				response = CommHelpers.readResponse(connection);
+				JSONParser parser = new JSONParser();
+				JSONObject json = (JSONObject)parser.parse(response);
+					
+				// actually a min id..
+				JSONArray statuses = (JSONArray)json.get("statuses");
+				Iterator<?> objects = statuses.listIterator();
+				while (objects.hasNext()) {
+					long id = (long)((JSONObject)objects.next()).get("id");
+					if (id < maxId||maxId == 0) maxId = id;
+				}
+		
+				String tweets = response.substring(13, response.indexOf("\"search_metadata\":{")-1) + "}";
+				results += tweets.substring(0, tweets.length()-2);
+				if (i < 9) results += ",";
+			}
+			results = "[" + results + "]";
 			
+			File f = new File("twitter_data.json");
+			if (f.exists()) f.delete();			
 			FileWriter file = new FileWriter("twitter_data.json");
-			file.write(CommHelpers.readResponse(connection));
-			file.close();
+			file.write(results);
+			file.close();			
 		}
 		catch (MalformedURLException e) {
 			throw new IOException("Invalid endpoint URL specified.", e);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		finally {
 			if (connection != null) {
